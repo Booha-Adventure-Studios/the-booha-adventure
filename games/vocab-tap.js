7b15330b7185c2906a5479a7501f7ce508a24f7f
Booha Adventure Studios <bryans.english@gmail.com>
2026-03-10 11:25:32 +0900

    Update vocab-tap.js

--- games/vocab-tap.js
+++ games/vocab-tap.js
@@ -1,1 +1,11 @@
 
+const app = document.getElementById("app");
+const ctx = window.BOOHA_GAME_CONTEXT;
+
+app.innerHTML = `
+  <div style="padding:24px;color:white;">
+    <h1>${ctx.title}</h1>
+    <p>${ctx.weekId}</p>
+    <p>${ctx.contentPath}</p>
+  </div>
+`;
