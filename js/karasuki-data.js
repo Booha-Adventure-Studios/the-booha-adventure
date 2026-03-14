f8a823032f94617c30bbcf0b1de6bd319de689b6
Booha Adventure Studios <bryans.english@gmail.com>
2026-03-14 15:03:52 +0900

    Update karasuki-data.js

--- js/karasuki-data.js
+++ js/karasuki-data.js
@@ -1,71 +1,145 @@
 
 window.KARASUKI_DATA = {
+  startRoom: "entrance",
 
-startRoom: "entrance",
+  rooms: {
+    entrance: {
+      bg: "assets/karasuki/rooms/entrance.png",
 
-rooms:{
+      spawns: {
+        default: { x: 180, y: 390 },
+        fromRight: { x: 780, y: 390 },
+        fromUp: { x: 470, y: 430 }
+      },
 
-entrance:{
+      exits: {
+        right: { to: "archives_hall", spawn: "fromLeft", dir: "right" },
+        up: { to: "forest_crossing", spawn: "fromDown", dir: "up" }
+      },
 
-bg:"assets/karasuki/rooms/entrance.png",
+      collisions: [
+        { x: 0, y: 0, w: 960, h: 40 },
+        { x: 0, y: 500, w: 960, h: 40 },
+        { x: 0, y: 0, w: 50, h: 540 },
+        { x: 910, y: 0, w: 50, h: 540 },
+        { x: 300, y: 120, w: 180, h: 110 }
+      ],
 
-spawns:{
-default:{x:200,y:380}
-},
+      hotspots: [
+        {
+          id: "archivesSign",
+          x: 690,
+          y: 180,
+          w: 150,
+          h: 180,
+          prompt: "ENTER ARCHIVES",
+          action: "overlay",
+          target: "archives"
+        }
+      ]
+    },
 
-exits:{
-right:{to:"archives_hall",spawn:"left"}
-},
+    archives_hall: {
+      bg: "assets/karasuki/rooms/archives_hall.png",
 
-hotspots:[
-{
-id:"archivesDoor",
-x:700,
-y:200,
-w:150,
-h:200,
-action:"overlay",
-target:"archives"
-}
-]
+      spawns: {
+        default: { x: 100, y: 390 },
+        fromLeft: { x: 100, y: 390 },
+        fromRight: { x: 780, y: 390 }
+      },
 
-},
+      exits: {
+        left: { to: "entrance", spawn: "fromRight", dir: "left" }
+      },
 
-archives_hall:{
+      collisions: [
+        { x: 0, y: 0, w: 960, h: 40 },
+        { x: 0, y: 500, w: 960, h: 40 },
+        { x: 0, y: 0, w: 50, h: 540 },
+        { x: 910, y: 0, w: 50, h: 540 },
+        { x: 390, y: 90, w: 180, h: 260 }
+      ],
 
-bg:"assets/karasuki/rooms/archives_hall.png",
+      hotspots: [
+        {
+          id: "archivesDoor",
+          x: 410,
+          y: 110,
+          w: 140,
+          h: 220,
+          prompt: "OPEN ARCHIVES",
+          action: "overlay",
+          target: "archives"
+        }
+      ]
+    },
 
-spawns:{
-default:{x:100,y:380},
-left:{x:80,y:380}
-},
+    forest_crossing: {
+      bg: "assets/karasuki/rooms/forest_crossing.png",
 
-exits:{
-left:{to:"entrance",spawn:"default"}
-},
+      spawns: {
+        default: { x: 470, y: 420 },
+        fromDown: { x: 470, y: 420 }
+      },
 
-hotspots:[
-{
-id:"archives",
-x:420,
-y:140,
-w:120,
-h:200,
-action:"overlay",
-target:"archives"
-}
-]
+      exits: {
+        down: { to: "entrance", spawn: "fromUp", dir: "down" }
+      },
 
-}
+      collisions: [
+        { x: 0, y: 0, w: 960, h: 40 },
+        { x: 0, y: 500, w: 960, h: 40 },
+        { x: 0, y: 0, w: 50, h: 540 },
+        { x: 910, y: 0, w: 50, h: 540 }
+      ],
 
-},
+      hotspots: [
+        {
+          id: "coreGamesMarker",
+          x: 640,
+          y: 140,
+          w: 160,
+          h: 180,
+          prompt: "CORE GAMES",
+          action: "goto",
+          href: "game-index.html"
+        }
+      ]
+    }
+  },
 
-overlays:{
-
-archives:{
-image:"assets/karasuki/rooms/archives_overlay.png"
-}
-
-}
-
+  overlays: {
+    archives: {
+      image: "assets/karasuki/rooms/archives_overlay.png",
+      hotspots: [
+        {
+          id: "archiveGames",
+          x: 10,
+          y: 22,
+          w: 19,
+          h: 38,
+          action: "goto",
+          href: "game-index.html"
+        },
+        {
+          id: "archiveStudy",
+          x: 34,
+          y: 22,
+          w: 19,
+          h: 38,
+          action: "goto",
+          href: "study-index.html"
+        },
+        {
+          id: "archiveLore",
+          x: 58,
+          y: 22,
+          w: 19,
+          h: 38,
+          action: "popup",
+          message: "Later this can open a note, memory drawer, or lore shelf."
+        }
+      ]
+    }
+  }
 };
