218aea7b0ad6ca64ad8982377ddf97e32926ea9b
Booha Adventure Studios <bryans.english@gmail.com>
2026-03-25 14:45:15 +0900

    Update karasuki.js

--- js/karasuki.js
+++ js/karasuki.js
@@ -972,7 +972,7 @@
     } else {
       descEl.textContent = tree.descLocked; descJpEl.textContent = tree.descLockedJP; descKEl.textContent = tree.descLockedKanji;
     }
-    descEl.style.color = '#ffffff'; descJpEl.style.color = '#ffffff'; descKEl.style.color = '#a888cc';
+    descEl.style.color = '#ffffff'; descJpEl.style.color = '#ffffff'; descKEl.style.color = '#ffffff';
 
     const btnsEl = document.getElementById('bonus-pop-btns');
     const okEl   = document.getElementById('bonus-pop-ok');
