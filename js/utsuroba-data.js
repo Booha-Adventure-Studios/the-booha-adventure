65be95f304f5bff674f4dcfd168c91b5f6dcbe88
Booha Adventure Studios <bryans.english@gmail.com>
2026-03-30 14:18:29 +0900

    Update utsuroba-data.js

--- js/utsuroba-data.js
+++ js/utsuroba-data.js
@@ -23,6 +23,7 @@ window.UTSUROBA_DATA = {
       sprite2     : './assets/img/drifters/kurobane_shizuma-2.png',
       audioPrefix : 'ks',
       memoryCount : 2,
+      scale       : 3.0, 
       /* x, y in world coords — used for whichever room is assigned */
       roomCoords  : { x: 900, y: 480 }
     },
@@ -37,6 +38,7 @@ window.UTSUROBA_DATA = {
       sprite2     : './assets/img/drifters/ned-the-oogle-2.png',
       audioPrefix : 'nto',
       memoryCount : 2,
+      scale       : 4.0, 
       roomCoords  : { x: 650, y: 550 }
     },
     {
@@ -50,6 +52,7 @@ window.UTSUROBA_DATA = {
       sprite2     : './assets/img/drifters/chagrin_gobito-2.png',
       audioPrefix : 'cg',
       memoryCount : 2,
+      scale       : 1.0, 
       roomCoords  : { x: 820, y: 580 }
     }
   ],
