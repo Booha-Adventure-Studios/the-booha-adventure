987b7c4ec420f76ef673779ffe1a5b3f3577612f
Booha Adventure Studios <bryans.english@gmail.com>
2026-07-05 12:03:27 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,9 +5,9 @@
 // ============================================================
 
 const CURRENT_CACHES = {
-  pages:  'booha-pages-2026-23',
-  assets: 'booha-assets-2026-23',
-  decks:  'booha-decks-2026-23',
+  pages:  'booha-pages-2026-24',
+  assets: 'booha-assets-2026-24',
+  decks:  'booha-decks-2026-24',
 };
 
 const PAGE_CACHE  = CURRENT_CACHES.pages;
