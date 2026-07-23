cec8d0791ba0ff7dfa9c87bc60abf919e04d777a
Booha Adventure Studios <bryans.english@gmail.com>
2026-07-23 15:03:47 +0900

    Update cache version numbers in sw.js

--- sw.js
+++ sw.js
@@ -5,9 +5,9 @@
 // ============================================================
 
 const CURRENT_CACHES = {
-  pages:  'booha-pages-2026-39',
-  assets: 'booha-assets-2026-39',
-  decks:  'booha-decks-2026-39',
+  pages:  'booha-pages-2026-40',
+  assets: 'booha-assets-2026-40',
+  decks:  'booha-decks-2026-40',
 };
 
 const PAGE_CACHE  = CURRENT_CACHES.pages;
