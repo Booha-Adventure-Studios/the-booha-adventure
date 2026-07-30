1870c5f8e2a33b52f6b4c8cba4a5e4ab0f86bc44
Uhibon <bryans.english@gmail.com>
2026-07-30 17:25:16 +0900

    particle fix in study decks
    The offline cache is updated to 2026-62 too

--- sw.js
+++ sw.js
@@ -5,9 +5,9 @@
 // ============================================================
 
 const CURRENT_CACHES = {
-  pages:  'booha-pages-2026-61',
-  assets: 'booha-assets-2026-61',
-  decks:  'booha-decks-2026-61',
+  pages:  'booha-pages-2026-62',
+  assets: 'booha-assets-2026-62',
+  decks:  'booha-decks-2026-62',
 };
 
 const PAGE_CACHE  = CURRENT_CACHES.pages;
